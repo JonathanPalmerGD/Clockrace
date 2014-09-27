b9087b112e9095e17ded3ac2391b0846d2d5f858
unknown <vantharion@gmail.com>
2014-09-27 19:21:57 -0400

    Basic idea prototyping

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour 
+{
+	public static bool paused = false;
+
+	public Rect playerGUI;
+	public float points;
+	public Time timeLeft;
+
+	void OnGUI()
+	{
+		playerGUI = GUI.Window(0, playerGUI, PlayerUI, "Clock Race!");
+	}
+
+	void PlayerUI(int windowID)
+	{
+		GUI.Label(new Rect(10, 20, 150, 40), "Points: " + points);
+	}
+
+	void Start () 
+	{
+		float windowWidth = 600;
+		playerGUI = new Rect(Screen.width / 2 - windowWidth / 2, 35, windowWidth, 60);
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	public Vector3 axisOfRotation = Vector3.up;
+	public float rotationRate = 10;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		transform.Rotate(axisOfRotation, rotationRate * Time.deltaTime);
+	}
+}
