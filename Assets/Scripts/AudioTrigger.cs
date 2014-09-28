890a7450f99df546b53e2bc2b4d2eab8a387375f
unknown <vantharion@gmail.com>
2014-09-28 00:11:24 -0400

    Created Audio! Still need hurt noises and warp noises.

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrigger : MonoBehaviour 
+{
+	public AudioClip[] clip;
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player")
+		{
+			if (clip != null && clip.Length > 0)
+			{
+				int randIndex = Random.Range(0, clip.Length);
+				AudioSource.PlayClipAtPoint(clip[randIndex], transform.position);
+			}
+		}
+	}
+}
